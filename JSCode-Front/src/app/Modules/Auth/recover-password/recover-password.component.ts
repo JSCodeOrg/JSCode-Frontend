725c5b2b8676785d988d5a449d99227e9be4964d
JuanCDiazV <juancamilodiazvalencia66@gmail.com>
2025-04-30 13:51:51 -0500

    Implementación de interfaces para restablecimiento de contraseñas

---
+++ JSCode-Front/src/app/Modules/Auth/recover-password/recover-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover-password',
+  imports: [],
+  templateUrl: './recover-password.component.html',
+})
+export class RecoverPasswordComponent {
+
+}
