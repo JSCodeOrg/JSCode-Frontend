0949b22cacf6bfa5de293a1b5987cc9da39e3ee7
Juandavid611 <rojasnarvaezjuandavid0611@gmail.com>
2025-05-08 16:43:26 -0500

    Implementacion de la interfaz de la actualizacion de perfil

--- JSCode-Front/src/app/Modules/Auth/profile-edit/profile-edit.component.ts
+++ JSCode-Front/src/app/Modules/Auth/profile-edit/profile-edit.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
-  imports: [],
   templateUrl: './profile-edit.component.html',
-  styleUrl: './profile-edit.component.css'
 })
-export class ProfileEditComponent {
+export class ProfileEditComponent implements OnInit {
+  profileForm!: FormGroup;
+  photoPreview: string | null = null;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      firstName: ['Mario', Validators.required],
+      lastName: ['Ocoró Pripra', Validators.required],
+      email: ['negrito@hotmail.com', [Validators.required, Validators.email]],
+      password: ['*', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      const updatedData = this.profileForm.value;
+      console.log('Datos actualizados:', updatedData);
+      // Aquí conectas con un servicio para guardar los datos
+    }
+  }
+
+  onChangePhoto() {
+    // Lógica para cargar nueva foto (input hidden + FileReader)
+  }
+
+  onDeletePhoto() {
+    this.photoPreview = null;
+  }
 }
