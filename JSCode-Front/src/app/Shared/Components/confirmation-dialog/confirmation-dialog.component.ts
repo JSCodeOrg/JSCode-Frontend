4f93d03ef66a8cf6eabd7b708fa93d29696512e2
JuanCDiazV <juancamilodiazvalencia66@gmail.com>
2025-05-21 17:36:46 -0500

    interfaces de eliminacion de productos

--- JSCode-Front/src/app/Shared/Components/confirmation-dialog/confirmation-dialog.component.ts
+++ JSCode-Front/src/app/Shared/Components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/components/button/button.component";
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [],
   templateUrl: './confirmation-dialog.component.html',
-  styleUrl: './confirmation-dialog.component.css'
+  styleUrls: ['./confirmation-dialog.component.scss'],
+  imports: [ButtonComponent]
 })
 export class ConfirmationDialogComponent {
+  confirmar() {
+    console.log('Acción confirmada');
+  }
 
+  cancelar() {
+    console.log('Acción cancelada');
+  }
+  confirmarAccion (event:Event){return} 
+  cancelarAccion (event:Event){return} 
 }
